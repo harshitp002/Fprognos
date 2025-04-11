64ac2a62cfa1fcd36e6d1cd1f140d1073723a641
Atu77l <kesharwaniatul9935@gmail.com>
2025-04-11 11:22:38 +0530

    Tailwind setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-import { Dashboard } from "./component/features/dashboard/Dashboard";
+import { Dashboard } from "./component/features/dashboard/Dashboard"; // ✅ Check correct path & folder name
+import React from "react";
 
 function App() {
-  return <Dashboard />;
+  return (
+    <div>
+      <Dashboard />
+      </div>
+  );
 }
 
 export default App;
